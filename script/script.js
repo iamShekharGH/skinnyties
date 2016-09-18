52597e8d586ef866ee6a880b484139e6e8fa194c
iamShekharGH <shekhargh@shekhargh.com>
2016-09-18 22:12:03 +0530

    Added a hover popup menu on the collections

--- script/script.js
+++ script/script.js
@@ -4,26 +4,44 @@ var collectio = document.getElementById('collection');
 var collectionddow = document.getElementsByClassName('collectionddown');
 var container = document.getElementsByClassName('container');
 
-// container[0].addEventListener("click" ,function(event){
-// 	console.log(event);
-// 	collectionddow[0].style.display = 'none';
+// collectio.addEventListener("mouseover" ,function(event){
+// 	// collectionddow.style+="display=flex;";
+// 	collectionddow[0].style.display = collectionddow[0].style.display === 'none' ? '' : 'none';
+
+// 	collectionddow[0].addEventListener("mouseout",function(event){
+
+// 		collectionddow[0].style.display = collectionddow[0].style.display === 'none' ? '' : 'none';
+// 	});
+	
+
+// 	//document.getElementsByClassName('collectionddown')[0].style.display = "flex" ;
+// 	console.log(collectionddow[0].style);
+// 	//collectionddow.style = "display: flex;";
+// 	//this.style = "background-color:red;";
+	
+	
+
 
 // });
 
-collectio.addEventListener("click" ,function(event){
-	// collectionddow.style+="display=flex;";
-	collectionddow[0].style.display = collectionddow[0].style.display === 'none' ? '' : 'none';
+// collectionddow[0].addEventListener("mouseout" ,function(event){
+// 	// collectionddow.style+="display=flex;";
+// 	collectionddow[0].style.display = collectionddow[0].style.display === 'none' ? '' : 'none';
 	
-
-	//document.getElementsByClassName('collectionddown')[0].style.display = "flex" ;
-	console.log(collectionddow[0].style);
-	//collectionddow.style = "display: flex;";
-	//this.style = "background-color:red;";
 	
 	
 
+// 	//document.getElementsByClassName('collectionddown')[0].style.display = "flex" ;
+// 	console.log(collectionddow[0].style);
+// 	//collectionddow.style = "display: flex;";
+// 	//this.style = "background-color:red;";
+	
+	
 
-});
+
+// });
+
+
 
 
 
